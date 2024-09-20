3d27c03bb111fe49b2bba82a4c3807e259bd6c6a
jhks777 <jhansi.priya@think41.com>
2024-09-20 11:11:48 +0530

    US-5-FT-1--Create a profile creation and editing component for job seekers.

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import JobApplicationPage from './pages/JobApplicationPage';
 import ApplicationTrackingPage from './pages/ApplicationTrackingPage';
 import EditApplicationPage from './pages/EditApplicationPage';
 import ApplicantManagementPage from './pages/ApplicantManagementPage';
+import ProfileEditPage from './pages/ProfileEditPage';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Route path="/application-tracking" element={<ApplicationTrackingPage />} />
         <Route path="/edit-application" element={<EditApplicationPage />} />
         <Route path="/applicant-management" element={<ApplicantManagementPage />} />
+        <Route path="/profile-edit" element={<ProfileEditPage />} />
       </Routes>
       <Footer />
     </Router>
